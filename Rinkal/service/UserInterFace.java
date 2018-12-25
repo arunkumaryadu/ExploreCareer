c042bfca02f54c1e1f9bca15fb1d1d83c0dd7bf2
Rinkal-jain <46125477+Rinkal-jain@users.noreply.github.com>
2018-12-25 15:36:12 +0530

    Add files via upload

---
+++ Rinkal/service/UserInterFace.java
@@ -0,0 +1,23 @@
+package service;
+
+import java.util.List;
+
+import model.Branch;
+import model.Users;
+
+public interface UserInterFace
+{
+	public boolean checkConfiguration();
+	public int registerUser(Users user);
+	public int loginUser(Users user);
+	public int updateUser(Users user);
+	public int deleteUser(Users user);
+	public Users searchUserByEmail(String emailid);
+	public List<Users> getUserList();
+	
+	
+
+}
+
+
+
