c5f756cc3403b0fb92ef53786fc4e4c70877be1c
ujjwaljesani <44519822+ujjwaljesani@users.noreply.github.com>
2018-12-27 01:15:04 +0530

    Add files via upload

---
+++ Ujjwal/model/Expert.java
@@ -0,0 +1,31 @@
+package model;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Expert")
+public class Expert extends Users {
+	
+	public String expertIn;
+	public Float expertSalary;
+
+	
+
+	public Float getExpertSalary() {
+		return expertSalary;
+	}
+
+	public void setExpertSalary(Float expertSalary) {
+		this.expertSalary = expertSalary;
+	}
+
+	public String getExpertIn() {
+		return expertIn;
+	}
+
+	public void setExpertIn(String expertIn) {
+		this.expertIn = expertIn;
+	}
+
+}
