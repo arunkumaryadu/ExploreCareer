47af4dfc31e2a50a33f3ffd5c9041cd8c4290ce2
Arun Kumar <arun94255@gmail.com>
2018-12-27 01:06:26 +0530

    Create CheckConfigurationTest.java

---
+++ Arun/test/java/branchUnitTest/CheckConfigurationTest.java
@@ -0,0 +1,45 @@
+package branchUnitTest;
+import javax.transaction.Transactional;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import config.HibernateConfig;
+import service.BranchServiceImpl;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=HibernateConfig.class, loader=AnnotationConfigContextLoader.class)
+@Transactional
+public class CheckConfigurationTest {
+	
+	@Autowired
+	@Qualifier("ml")
+	BranchServiceImpl branchServiceImpl;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	@Before
+	public void setUp() throws Exception {
+	}
+	@After
+	public void tearDown() throws Exception {
+	}
+	@Test
+	public void test() {
+	
+		boolean val=branchServiceImpl.checkConfiguration();
+		System.out.println(val);
+		Assert.assertEquals(true,val);
+	}
+}
